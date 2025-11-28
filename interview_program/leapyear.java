5dc86fff97ca8faa0e25dc37c5a26f10b693039b
Afzal Ahmed <mdafzalahmed5984@gmail.com>
2025-11-28 15:16:32 +0530

    3rd commit on interview program

---
+++ interview_program/leapyear.java
@@ -0,0 +1,10 @@
+//leap year 
+public class Leapyear{
+	public static void main(String[] args){
+	int year = 2024;
+	if (year%4 == 0 ){
+		System.out.println("leapyear is :"+ year );}
+	else{
+		System.out.println("not a leapyear:"+ year );}
+		}
+	} 
---
+++ interview_program/pract_ques.java
@@ -0,0 +1,11 @@
+//coding quesion
+
+public class Pract_ques{
+	public static void main (String[] args){
+		int x =2,y=3;
+		if (x++ > 4 || ++y > 3){
+			x+=2;
+		}
+		System.out.println(x+y);
+	}
+}
