b811b01ac011d0bc13df56a024768c4646d3f6fe
Afzal Ahmed <mdafzalahmed5984@gmail.com>
2025-12-11 11:54:09 +0530

    commit to add patterns

---
+++ interview_program/patterns/pattern_24.java
@@ -0,0 +1,23 @@
+public class pattern_24{
+		public static void main (String[] args){
+			int n=4;
+			for (int i=1;i<=n;i++){
+				for (int j=1;j<=n-i;j++){
+					System.out.print(" ");
+					}
+				char ch = 'A';
+				for (int j=1;j<=i;j++){
+					System.out.print(ch++);
+				}
+				ch--;
+				for (int j=i-1;j>0;j--){
+					System.out.print(--ch);
+				}
+			System.out.println();
+			}
+		}
+	}
+//   A
+//  ABA
+// ABCBA
+//ABCDCBA
